0949bccec6d0e68c51cce9de434d4caed7e0f481
RUBEN PEÑA PARADA <46791460+rubenpenap@users.noreply.github.com>
2025-07-31 14:51:59 -0400

    Refactor createSet function to set a default generic type and standardize quote usage.

--- src/02-passing-type-arguments/08-default-generics.problem.ts
+++ src/02-passing-type-arguments/08-default-generics.problem.ts
@@ -1,7 +1,7 @@
-import { Equal, Expect } from "../helpers/type-utils";
+import { Equal, Expect } from '../helpers/type-utils';
 
-export const createSet = <T>() => {
-  return new Set<T>();
+export const createSet = <T = string>() => {
+	return new Set<T>();
 };
 
 const numberSet = createSet<number>();
@@ -9,7 +9,7 @@ const stringSet = createSet<string>();
 const otherStringSet = createSet();
 
 type tests = [
-  Expect<Equal<typeof numberSet, Set<number>>>,
-  Expect<Equal<typeof stringSet, Set<string>>>,
-  Expect<Equal<typeof otherStringSet, Set<string>>>
+	Expect<Equal<typeof numberSet, Set<number>>>,
+	Expect<Equal<typeof stringSet, Set<string>>>,
+	Expect<Equal<typeof otherStringSet, Set<string>>>,
 ];
